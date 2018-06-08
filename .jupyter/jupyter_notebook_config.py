d24159a62a61bddf72ee8a2dbf6a7f7bd75488f5
Yukino Ikegami <yknikgm@gmail.com>
2018-06-09 05:32:11 +0900

    Add Jupyter notebook config

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -2,5 +2,10 @@
 import sys
 
 
+# Allowed IP
+c.NotebookApp.ip = '*'
+# Password
+c.NotebookApp.password = 'sha1:e9d9dd1458e6:67dbfdf680315c697318a767c4e30943ca6eae8e'
+
 if sys.platform == 'darwin':
     c.NotebookApp.browser = u'open -a /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome %s'
