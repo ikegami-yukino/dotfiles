834560690aa2fea66bad7d9dfede55f7d7a0bcf4
IKEGAMI Yukino <yknikgm@gmail.com>
2021-10-05 14:30:54 +0900

    Homebrewのインストールコマンド変更

--- install.sh
+++ install.sh
@@ -55,7 +55,7 @@ if [ "$(uname)" == 'Darwin' ]; then
     :
   else
     echo 'Install Homebrew'
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   fi
 
   echo 'Install apps by Homebrew'
